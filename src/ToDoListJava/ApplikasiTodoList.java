aa03281813d9c2a2d1cb92455d03a6cd58f239c2
muhammad.fikran <muhammadfikran@Muhammad-Fikran.local>
2024-07-14 10:05:31 +0700

    Todo List Java, reference dari PZN

--- src/ToDoListJava/ApplikasiTodoList.java
+++ src/ToDoListJava/ApplikasiTodoList.java
@@ -5,7 +5,7 @@ public class ApplikasiTodoList {
     public static java.util.Scanner scanner = new java.util.Scanner(System.in);
 
     public static void main(String[] args) {
-        testInput();
+        viewMenampilkanTodoList();
     }
 
     public static void menampilkanTodoList(){
